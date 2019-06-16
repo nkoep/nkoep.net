0dc9d26b162bb3bf9a163d44b905ee67ab0564e1
Niklas Koep <niklas.koep@gmail.com>
2019-06-16 11:15:31 +0200

    Initial commit

---
+++ source/conf.py
@@ -0,0 +1,25 @@
+project = "nkoep.net"
+author = "Niklas Koep"
+copyright = f"2019, {author}"
+
+extensions = ["sphinx.ext.imgmath"]
+
+templates_path = ['_templates']
+
+exclude_patterns = ["_build"]
+
+html_theme = "alabaster"
+html_title = "nkoep.net"
+html_static_path = ['_static']
+html_sidebars = {}
+html_domain_indices = True
+html_show_sourcelink = False
+html_show_sphinx = False
+show_powered_by = False
+html_show_copyright = False
+html_add_permalinks = "#"
+
+# imgmath extension options
+imgmath_image_format = "svg"
+imgmath_add_tooltips = False
+imgmath_latex_preamble = "\\usepackage[boldvectors,boldmatrices]{blatex}"
---
+++ source/index.rst
@@ -0,0 +1,21 @@
+nkoep.net
+=========
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Since Pythagoras, we know that :math:`a^2 + b^2 = c^2`.
+
+.. math::
+   :label: lin
+
+   \vmy = \vmA\vmx
+
+.. math::
+
+   \Minimize[\vmx]{\norm{\vmx}_1}{
+      \vmy = \vmA\vmx
+   }
+
+Note that :eq:`lin` is cool!
