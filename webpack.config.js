25134328451046bddee264c366217f5b6db79453
Niklas Koep <niklas.koep@gmail.com>
2019-12-27 16:14:46 +0100

    Don't use style-loader in production
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,79 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const staticAssets = ["pages", "posts"].map(folder => {
-  return {from: `./${folder}`, to: `./${folder}`}
-});
-["favicon.ico", "posts.toml"].forEach(
-  asset => staticAssets.push({from: `./${asset}`}));
+module.exports = (env, argv) => {
+  const devMode = argv.mode !== "production";
 
-module.exports = (env, argv) => ({
-  mode: "development",
-  entry: ["./src/index.js", "./src/main.scss"],
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "app.js",
-    publicPath: "/"
-  },
-  devtool: argv.mode === "production" ? false : "source-map",
-  devServer: {
-    contentBase: "./public",
-    historyApiFallback: {
-      rewrites: [{
-        from: /^\/404$/,
-        to: "/404.html"
-      }]
-    }
-  },
-  watch: false,
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              ["@babel/preset-env", {modules: false}]
-            ]
-          }
-        }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
+  const staticAssets = ["pages", "posts"].map(folder => {
+    return {from: `./${folder}`, to: `./${folder}`}
+  });
+  ["favicon.ico", "posts.toml"].forEach(
+    asset => staticAssets.push({from: `./${asset}`}));
+
+  return {
+    mode: "development",
+    entry: ["./src/index.js", "./src/main.scss"],
+    output: {
+      path: path.resolve(__dirname, "public"),
+      filename: "app.js",
+      publicPath: "/"
+    },
+    devtool: devMode ? "source-map" : false,
+    devServer: {
+      contentBase: "./public",
+      historyApiFallback: {
+        rewrites: [{
+          from: /^\/404$/,
+          to: "/404.html"
+        }]
       }
+    },
+    watch: false,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                ["@babel/preset-env", {modules: false}]
+              ]
+            }
+          }
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "postcss-loader",
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new CopyWebpackPlugin(staticAssets),
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/templates/index.html",
+        filename: "index.html"
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/templates/404.html",
+        filename: "404.html",
+        inject: false
+      })
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./src/templates/index.html",
-      filename: "index.html"
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/templates/404.html",
-      filename: "404.html",
-      inject: false
-    }),
-    new CopyWebpackPlugin(staticAssets)
-  ]
-});
+  }
+};
