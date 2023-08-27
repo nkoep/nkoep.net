d69b1badb3c5869a1e537dbcb713a4736d45cd29
Niklas Koep <niklas.koep@gmail.com>
2023-08-27 13:30:15 +0200

    Replace dead twitter link with email
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- src/components/Social.svelte
+++ src/components/Social.svelte
@@ -4,18 +4,28 @@
     faGithub,
     faInstagram,
     faLastfm,
-    faTwitter
   } from "@fortawesome/free-brands-svg-icons";
+  import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
   export let iconScale = 1;
 
   const socialPages = [
     { url: "https://github.com/nkoep/", icon: faGithub },
     { url: "https://instagram.com/polylith_", icon: faInstagram },
-    { url: "https://twitter.com/spczf", icon: faTwitter },
-    { url: "https://last.fm/user/cRZYFST", icon: faLastfm }
+    { url: "https://last.fm/user/cRZYFST", icon: faLastfm },
+    { url: "mailto:contact@nkoep.net", icon: faEnvelope },
   ];
 </script>
+
+<ul>
+  {#each socialPages as page}
+    <li>
+      <a href={page.url} target="_blank"
+        ><Icon data={page.icon} scale={iconScale} /></a
+      >
+    </li>
+  {/each}
+</ul>
 
 <style lang="scss">
   @import "../style/theme.scss";
@@ -30,9 +40,3 @@
     white-space: nowrap;
   }
 </style>
-
-<ul>
-  {#each socialPages as page}
-    <li><a href="{page.url}" target="_blank"><Icon data={page.icon} scale="{iconScale}"/></a></li>
-  {/each}
-</ul>
