060f9e64b9810ff046da6ce68f7a4ee5e15a5f46
Niklas Koep <niklas.koep@gmail.com>
2024-11-24 10:57:55 +0100

    Add simple header animation
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- src/routes/Logo.svelte
+++ src/routes/Logo.svelte
@@ -5,18 +5,39 @@
 <style lang="scss">
   @use "./theme.scss";
 
-  a {
-    color: theme.$fg;
-
-    &:hover {
-      color: theme.$link;
-    }
-  }
-
   div {
     font-family: "Cormorant", serif;
     font-size: 40px;
     margin: auto 0;
     text-align: center;
+  }
+
+  a {
+    color: theme.$fg;
+    display: inline-block;
+    padding: 0.1em 0;
+    position: relative;
+    text-decoration: none;
+
+    &::after {
+      background-color: theme.$link;
+      bottom: 0;
+      content: "";
+      height: 2px;
+      left: 0;
+      position: absolute;
+      transform-origin: right;
+      transform: scaleX(0);
+      transition: transform 250ms;
+      width: 100%;
+    }
+
+    &:hover,
+    &:active {
+      &::after {
+        transform: scaleX(1);
+        transform-origin: left;
+      }
+    }
   }
 </style>
